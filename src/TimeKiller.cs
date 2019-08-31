30e611e994c4c398805ff57b7dc0d5912a2b70d0
piuy11 <34086332+piuy11@users.noreply.github.com>
2019-08-31 23:18:45 +0900

    Update TimeKiller.cs

--- src/TimeKiller.cs
+++ src/TimeKiller.cs
@@ -27,7 +27,7 @@ namespace TimeKiller
                 Console.WriteLine("3. 로또 추첨기");
                 Console.WriteLine("4. 오목");
                 
-                switch(Console.ReadKey().KeyChar)
+                switch(Console.ReadKey(true).KeyChar)
                 {
                     case '1':
                         BeARich.Game();
@@ -53,6 +53,11 @@ namespace TimeKiller
         public const string BASIC_PATH = TimeKiller.BASIC_PATH + @"\BeARich";
         public const string SCOREBOARD_PATH = BASIC_PATH + @"\scoreborad.dat";
         public const string SAVE_PATH = BASIC_PATH + @"save.dat";
+        public const string PATCH_NOTE = GAME_VERSION + @" 패치 노트
+1. 심심풀이 프로그램에 탑재 완료
+2. 메뉴 추가
+3. 점수판 기능 추가
+4. 이어하기 기능 추가";
         
         
         private static Tuple<string, long>[] scoreBoard = new Tuple<string, long>[10];
@@ -60,35 +65,6 @@ namespace TimeKiller
         public static void Game()
         {
             BasicSet();
-            Intro();
-        }
-        
-        private static void BasicSet()
-        {
-            if (!File.Exists(SCOREBOARD_PATH)) {
-                BinaryWriter bw = new BinaryWriter(File.Open(SCOREBOARD_PATH, FileMode.CreateNew));
-                bw.Write("홍민준");
-                bw.Write(10000L);
-                bw.Close();
-            }
-                using (BinaryReader bw = new BinaryReader(File.Open(SCOREBOARD_PATH, FileMode.Open)))
-                {
-                    try {
-                        for (int i = 0; i < 10; ++i)
-                        {
-                            string name = bw.ReadString();
-                            long score = bw.ReadInt64();
-                            scoreBoard[i] = new Tuple<string, long>(name, score);
-                        }
-                    }
-                    catch (EndOfStreamException e) {
-                        // e();
-                    }
-                }
-        }
-        
-        public static long Intro()
-        {
             do
             {
                 Console.Clear();
@@ -96,49 +72,117 @@ namespace TimeKiller
                 Console.WriteLine("1. 새로하기");
                 Console.WriteLine("2. 이어하기");
                 Console.WriteLine("3. 점수판");
-                Console.WriteLine("4. 종료");
-                
-                Console.WriteLine(@"{0} 패치 내역
-                1", GAME_VERSION);
+                Console.WriteLine("4. 패치 노트");
+                Console.WriteLine("5. 종료");
                 
                 char choice = Console.ReadKey(true).KeyChar;
                 switch(choice)
                 {
                     case '1':
-                        return StartMoney;
+                        PlayNew();
+                        break;
                     case '2':
-                        return PlayContinue();
+                        PlayNew(PlayContinue());
+                        break;
                     case '3':
                         ShowScoreBoard();
                         break;
                     case '4':
-                        return -1;
+                        Console.Clear();
+                        Console.WriteLine(PATCH_NOTE);
+                        Console.WriteLine("PRESS ANY KEY TO CONTINUE");
+                        Console.ReadKey(true);
+                        break;
+                    case '5':
+                        return;
                 }
                     
             } while (true);
-            
         }
         
-        static long PlayNew()
+        private static void BasicSet()
         {
-            return 0;
+            if (!File.Exists(SCOREBOARD_PATH)) {
+                using (BinaryWriter bw = new BinaryWriter(File.Open(SCOREBOARD_PATH, FileMode.CreateNew)))
+                {
+                    bw.Write("홍민준");
+                    bw.Write(10000L);
+                    bw.Close();
+                    Console.WriteLine("점수판을 생성했습니다.");
+                    Console.ReadKey(true);
+                }
+            }
+                using (BinaryReader bw = new BinaryReader(File.Open(SCOREBOARD_PATH, FileMode.Open)))
+                {
+                    int i = 0;
+                    try {
+                        for (i = 0; i < 10; ++i)
+                        {
+                            string name = bw.ReadString();
+                            long score = bw.ReadInt64();
+                            scoreBoard[i] = new Tuple<string, long>(name, score);
+                        }
+                    }
+                    catch (EndOfStreamException e) {
+                        for (int j = i; j < 10; ++j)
+                            scoreBoard[j] = new Tuple<string, long>("", 0L);
+                    }
+                }
         }
         
-        static long PlayContinue()
+        static void PlayNew(long money = StartMoney)
         {
+            do {
+                Console.Clear();
+                Console.WriteLine("돈 : " + money);
+                Console.WriteLine("1. 배팅한 돈의 2배 : 50%");
+                Console.WriteLine("2. 배팅한 돈의 4배 : 25%");
+                Console.WriteLine("3. 배팅한 돈의 10배 : 10%");
+                Console.WriteLine("4. 배팅한 돈의 100배 : 1%");
+                Console.WriteLine("5. 노가다 : + 50");
+                Console.WrtieLine("6. 구구단 맞추기 : + 100");
+            } while (money > 0);
+        }
+        
+        static void PlayContinue()
+        {
+            long money;
+            using (BinaryReader br = new BinaryReader(File.Open(SAVE_PATH, FileMode.Open)))
+            {
+                money = br.ReadInt64();
+            }
             return 0;
         }
         
         static void ShowScoreBoard()
         {
             Console.Clear();
-            Console.WriteLine("점수판 : " + scoreBoard.Length);
+            Console.WriteLine("점수판\n");
+            int i = 1;
             foreach (var tuple in scoreBoard)
             {
-                Console.Write(tuple);
+                Console.WriteLine(i.ToString() + ' ' + tuple.Item2 + ' ' + tuple.Item1);
+                i++;
             }
             Console.ReadKey(true);
         }
         
+        static void WriteScoreBoard()
+        {
+            using (BinaryWriter bw = new BinaryWriter(File.Open(SCOREBOARD_PATH, FileMode.CreateNew)))
+            {
+                foreach (var tuple in scoreBoard)
+                {
+                    bw.Write(tuple.Item1);
+                    bw.Write(tuple.Item2);
+                }
+                bw.Close();
+            }
+        }
+        
+        static void Gamble()
+        {
+            
+        }
     }
 }
