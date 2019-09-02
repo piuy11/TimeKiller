b655de940f09266a9f4c08ebcc54ab804067623b
piuy11 <34086332+piuy11@users.noreply.github.com>
2019-09-02 22:33:07 +0900

    Update A2048.cs

--- src/A2048.cs
+++ src/A2048.cs
@@ -7,24 +7,13 @@ namespace A
     class A2048
     {
         private int[,] board;
-        private List<Tuple<int, int>> emptyList;
         
         public A2048()
         {
             board = new int[4, 4];
-            emptyList = new List<Tuple<int, int>>();
-            
             board.Initialize();
-            board[1, 3] = 4;
-            board[0, 3] = 2;
-            for (int i = 0; i < 4; ++i)
-            {
-                for (int j = 0; j < 4; ++j)
-                {
-                    emptyList.Add(new Tuple<int, int>(i, j));
-                }
-            }
-            emptyList.RemoveAt(0);
+
+			GenerateNewBlock();
         }
         
         public void Play()
@@ -49,64 +38,60 @@ namespace A
                 }
             }
         }
-        
-        private void MoveLine(ref List<int> list)
-        {
-            int empty = 4;
-            for (int i = 0; i < 4; ++i)
-            {
-                if (list[i] == 0 && empty == 4)
-                    empty = i;
-                else if (list[i] != 0 && empty != 4) {
-                    list[empty] = list[i];
-                    list[i] = 0;
-                    empty++;
-                }
-            }
-        }
-        
-        private void MoveLine()
-        {
-            int[] a = {1,2,3,4};
-            
-            
-        }
+
+		private void GenerateNewBlock()
+		{
+			List<Tuple<int, int>> emptyList = new List<Tuple<int, int>>();
+
+			for (int i = 0; i < 4; ++i)
+			{
+				for (int j = 0; j < 4; ++j)
+				{
+					if (board[i, j] == 0)
+						emptyList.Add(new Tuple<int, int>(i, j));
+				}
+			}
+
+			Random randomSeed = new Random();
+            var pair = emptyList[ randomSeed.Next(0, emptyList.Count()) ];
+            board[pair.Item1, pair.Item2] = (randomSeed.Next(0, 2) == 0 ? 2 : 4);
+		}
         
         private void MoveLeft()
         {
             bool moved = false;
             for (int i = 0; i < 4; ++i)
             {
-                int empty = 4;
+				List<int> emptyList = new List<int>();
                 for (int j = 0; j < 4; ++j)
                 {
-                    if (board[i, j] == 0 && empty == 4)
-                        empty = j;
-                    else if (board[i, j] != 0 && empty != 4) {
-                        if ()
-                        board[i, empty] = board[i, j];
+                    if (board[i, j] == 0)
+                        emptyList.Add(j);
+                    else if (emptyList.Count() != 0) {
+                        board[i, emptyList[0]] = board[i, j];
                         board[i, j] = 0;
-                        empty++;
+						emptyList.RemoveAt(0);
+						emptyList.Add(j);
                         moved = true;
                     }
                 }
+
+				for (int j = 0; j < 3; ++j)
+				{
+					if (board[i, j] == board[i, j + 1] && board[i, j] != 0) {
+						moved = true;
+						board[i, j] *= 2;
+						for (int k = j + 1; k < 3; ++k)
+						{
+							board[i, k] = board[i, k + 1];
+						}
+						board[i, 3] = 0;
+					}
+				}
             }
             
             if (moved) {
-                emptyList.Clear();
-                for (int i = 0; i < 4; ++i)
-                {
-                    for (int j = 0; j < 4; ++j)
-                    {
-                        if (board[i, j] == 0)
-                            emptyList.Add(new Tuple<int, int>(i, j));
-                    }
-                }
-                
-                Random randomSeed = new Random();
-                
-                var pair = emptyList[ randomSeed.Next(0, emptyList.Count()) ];
-                board[pair.Item1, pair.Item2] = (randomSeed.Next(0, 2) == 0 ? 2 : 4);
+                GenerateNewBlock();
             }
         }
         
@@ -208,12 +193,34 @@ namespace A
             Console.Clear();
             for (int i = 0; i < 4; ++i)
             {
+				for (int j = 0; j < 4; ++j)
+				{
+					Console.Write("|");
+					for (int k = 0; k < 7; ++k)
+					{
+						Console.Write("-");
+					}
+				}
+				Console.Write("|\n");
+
                 for (int j = 0; j < 4; ++j)
                 {
-                    Console.Write("{0, -7}", board[i, j]);
+					if (board[i,j] == 0)
+						Console.Write("|       ");
+					else
+						Console.Write("|{0, -7}", board[i, j]);
                 }
-                Console.Write("\n\n");
+                Console.Write("|\n");
             }
+
+			for (int j = 0; j < 4; ++j)
+				{
+					Console.Write("|");
+					for (int k = 0; k < 7; ++k)
+					{
+						Console.Write("-");
+					}
+				}
         }
     }
     
